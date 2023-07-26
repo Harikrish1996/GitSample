c95d1d56f4a6aec74dae2ce68a2a223a4acd3a59
Harikrish1996 <kharikrish98@gmail.com>
2023-07-26 16:25:02 +0530

    Done

---
+++ src/org/theni/Popeye.java
@@ -0,0 +1,16 @@
+package org.theni;
+
+public class Popeye {
+
+	public static void main(String[] args) {
+		int a =25;
+		System.out.println("A value ptint :" +a);
+		
+	System.out.println("A is work completed");
+		
+		
+	}
+	
+	
+	
+}
