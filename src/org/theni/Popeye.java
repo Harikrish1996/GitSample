a1e6b4f8966983b5968c6140876a6e987ace7e5c
Harikrish1996 <kharikrish98@gmail.com>
2023-07-26 16:47:21 +0530

    Gobi Work has been done

--- src/org/theni/Popeye.java
+++ src/org/theni/Popeye.java
@@ -1,5 +1,8 @@
 package org.theni;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Popeye {
 
 	public static void main(String[] args) {
@@ -7,6 +10,17 @@ public class Popeye {
 		System.out.println("A value ptint :" +a);
 		
 	System.out.println("A is work completed");
+	
+	List re = new LinkedList();
+	
+	re.add(100);
+	re.add(200);
+	re.add("Hari Krishana");
+	re.add(2154879653l);
+	re.add('2');
+	
+	System.out.println(re);
+	
 		
 		
 	}
